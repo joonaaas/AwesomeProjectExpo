cf8790244e5daaeb4549836d282af963863a140e
Jonas <jonasqalolino@gmail.com>
2022-12-12 19:51:53 +0800

    chore: implement navigation stack

---
+++ screens/ColorPalette.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native';
+import ColorBox from '../components/ColorBox';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
+
+export default function ColorPalette() {
+  return (
+    <SafeAreaView>
+      <View>
+        <FlatList
+          data={COLORS}
+          style={styles.container}
+          keyExtractor={(item, index) => index}
+          renderItem={({ item }) => (
+            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+          )}
+          ListHeaderComponent={<Text style={styles.header}>Transaction</Text>}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginBottom: 20,
+  },
+});
---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    marginBottom: 20,
+  },
+});
