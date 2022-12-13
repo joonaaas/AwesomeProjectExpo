da812097b376e5277616cecdd8e88822d87a6639
Jonas <jonasqalolino@gmail.com>
2022-12-13 19:14:17 +0800

    hooks exercise

--- components/SectionPalette.js
+++ components/SectionPalette.js
@@ -1,23 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 
-export default function SectionPalette({
-  hexCode = '#EEE',
-  colorName = 'Gray',
-}) {
-  const textColor = {
-    color:
-      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
-        ? 'black'
-        : 'white',
-  };
-  return <View style={[styles.container, { backgroundColor: hexCode }]} />;
+export default function SectionPalette({ paletteName, colors, handlePress }) {
+  // const textColor = {
+  //   color:
+  //     parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+  //       ? 'black'
+  //       : 'white',
+  // };
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <Text style={styles.header}>{paletteName}</Text>
+      <FlatList
+        data={colors.slice(0, 5)}
+        keyExtractor={(item, index) => index}
+        style={styles.colors}
+        horizontal={true}
+        renderItem={({ item }) => (
+          <View style={[styles.box, { backgroundColor: item?.hexCode }]}></View>
+        )}
+      />
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    marginRight: 6,
-    width: 40,
-    height: 38,
+  header: {
+    fontSize: 17,
+    fontWeight: '600',
+    marginBottom: 12,
+    width: '100%',
+  },
+  box: {
+    width: 30,
+    height: 30,
+    marginRight: 8,
+  },
+  colors: {
+    marginBottom: 10,
   },
 });
--- screens/Home.js
+++ screens/Home.js
@@ -1,110 +1,49 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  FlatList,
-} from 'react-native';
-
+import { StyleSheet, View, FlatList } from 'react-native';
+import { useEffect, useState, useCallback } from 'react';
 import SectionPalette from '../components/SectionPalette';
 
-const SOLARIZED = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-];
-
-const RAINBOW = [
-  { colorName: 'Red', hexCode: '#FF0000' },
-  { colorName: 'Orange', hexCode: '#FF7F00' },
-  { colorName: 'Yellow', hexCode: '#FFFF00' },
-  { colorName: 'Green', hexCode: '#00FF00' },
-  { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const FRONTEND_MASTERS = [
-  { colorName: 'Red', hexCode: '#c02d28' },
-  { colorName: 'Black', hexCode: '#3e3e3e' },
-  { colorName: 'Grey', hexCode: '#8a8a8a' },
-  { colorName: 'White', hexCode: '#ffffff' },
-  { colorName: 'Orange', hexCode: '#e66225' },
-];
-
 export default function Home({ navigation }) {
+  const [palettes, setPalettes] = useState([]);
+
+  const getPalettes = useCallback(async () => {
+    const data = await fetch(
+      'https://color-palette-api.kadikraman.vercel.app/palettes',
+    );
+    if (data.ok) {
+      const json = await data.json();
+      setPalettes(json);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPalettes();
+  }, [getPalettes]);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('ColorPalette', {
-            paletteName: 'Solarized',
-            colors: SOLARIZED,
-          });
+      <FlatList
+        data={palettes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <SectionPalette
+              paletteName={item.paletteName}
+              colors={item.colors}
+              handlePress={() =>
+                navigation.navigate('ColorPalette', {
+                  paletteName: item.paletteName,
+                  colors: item.colors,
+                })
+              }
+            />
+          );
         }}
-      >
-        <Text style={styles.header}>Solarized</Text>
-        <FlatList
-          data={SOLARIZED}
-          keyExtractor={(item, index) => item + index}
-          horizontal={true}
-          style={{ marginBottom: 20 }}
-          renderItem={({ item }) => (
-            <SectionPalette colorName={item.colorName} hexCode={item.hexCode} />
-          )}
-        />
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('ColorPalette', {
-            paletteName: 'Rainbow',
-            colors: FRONTEND_MASTERS,
-          });
-        }}
-      >
-        <Text style={styles.header}>Frontend Masters</Text>
-        <FlatList
-          data={FRONTEND_MASTERS}
-          keyExtractor={(item, index) => item + index}
-          horizontal={true}
-          style={{ marginBottom: 20 }}
-          renderItem={({ item }) => (
-            <SectionPalette colorName={item.colorName} hexCode={item.hexCode} />
-          )}
-        />
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('ColorPalette', {
-            paletteName: 'Rainbow',
-            colors: RAINBOW,
-          });
-        }}
-      >
-        <Text style={styles.header}>Rainbow</Text>
-        <FlatList
-          data={RAINBOW}
-          keyExtractor={(item, index) => item + index}
-          horizontal={true}
-          renderItem={({ item }) => (
-            <SectionPalette colorName={item.colorName} hexCode={item.hexCode} />
-          )}
-        />
-      </TouchableOpacity>
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    fontSize: 17,
-    fontWeight: '600',
-    marginBottom: 12,
-    width: '100%',
-  },
   container: {
     padding: 20,
     marginBottom: 20,
